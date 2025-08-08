38875e930bd5631e80de6aecb6870f8828644a59
kodonghee <qmdlfakrhf@naver.com>
2025-08-08 17:59:33 +0900

    refact: 도메인에서 웹 에러 호출 방지

--- apps/commerce-api/src/main/java/com/loopers/domain/brand/Brand.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/brand/Brand.java
@@ -1,8 +1,6 @@
 package com.loopers.domain.brand;
 
 import com.loopers.domain.BaseEntity;
-import com.loopers.support.error.CoreException;
-import com.loopers.support.error.ErrorType;
 import jakarta.persistence.*;
 
 @Entity
@@ -16,7 +14,7 @@ public class Brand extends BaseEntity {
 
     public Brand(String name) {
         if (name == null || name.isBlank()) {
-            throw new CoreException(ErrorType.BAD_REQUEST, "브랜드명은 필수입니다.");
+            throw new IllegalArgumentException("브랜드명은 필수입니다.");
         }
         this.name = name;
     }
