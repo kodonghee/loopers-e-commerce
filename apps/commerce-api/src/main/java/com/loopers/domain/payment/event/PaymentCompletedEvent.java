921f16a1364c010f8195001ae29a48ecb5ff4750
kodonghee <qmdlfakrhf@naver.com>
2025-08-29 16:18:27 +0900

    feat: 결제 완료 이벤트 필드 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/event/PaymentCompletedEvent.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/event/PaymentCompletedEvent.java
@@ -1,14 +1,17 @@
 package com.loopers.domain.payment.event;
 
+import com.loopers.domain.order.PaymentMethod;
+
 import java.math.BigDecimal;
 
 public record PaymentCompletedEvent(
         String orderId,
         String userId,
         BigDecimal amount,
-        Long couponId
+        Long couponId,
+        PaymentMethod paymentMethod
 ) {
-    public static PaymentCompletedEvent of(String orderId, String userId, BigDecimal amount, Long couponId) {
-        return new PaymentCompletedEvent(orderId, userId, amount, couponId);
+    public static PaymentCompletedEvent of(String orderId, String userId, BigDecimal amount, Long couponId, PaymentMethod paymentMethod) {
+        return new PaymentCompletedEvent(orderId, userId, amount, couponId, paymentMethod);
     }
 }
